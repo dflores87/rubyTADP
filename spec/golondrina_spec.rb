fd736de2465d3b1c64c1b5e327b17e7b7ba402f9
dflores87 <danielf_2008@hotmail.com>
2015-09-03 11:35:24 -0300

    subiendo prueba golondrina

---
+++ spec/golondrina_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require_relative '../src/Golondrina'
+
+describe 'golondrinas' do
+  let (:una_golondrina){
+    Golondrina.new
+  }
+
+  it 'deberia perder energia cdo vuela' do
+    una_golondrina.volar(10)
+    expect(una_golondrina.energia).to eq(900)
+
+  end
+
+  it 'deberia ganar energia cdo come' do
+
+  end
+end
